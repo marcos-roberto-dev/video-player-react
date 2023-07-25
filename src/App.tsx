cf47fd732cfbf0eb0167fbc7d010003cb1b98c11
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-07-25 17:30:55 -0300

    :fire: remove todo test redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Provider as ReduxProvider } from "react-redux";
 
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
 import { store } from "./store";
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+      <h1>Hello</h1>
     </ReduxProvider>
   );
 }
