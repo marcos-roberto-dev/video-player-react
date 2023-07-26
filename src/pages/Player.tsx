84416d6f0a22e3aec60c21ae311f5b148e5994c3
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-07-26 08:59:53 -0300

    :sparkles: create react player

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,3 +1,5 @@
+import ReactPlayer from "react-player";
+
 import { ClassItem } from "../components/ClassItem";
 import { Header } from "../components/Header";
 
@@ -7,8 +9,17 @@ export function Player() {
       <div className="flex w-[1100px] flex-col gap-6">
         <Header />
         <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
-          <div className="flex-1">video</div>
-          <aside className="w-80 border-l border-zinc-800 bg-zinc-900 h-[600px]">
+          <div className="flex-1">
+            <div className="w-full bg-zinc-950 aspect-video">
+              <ReactPlayer
+                width="100%"
+                height="100%"
+                controls
+                url="https://www.youtube.com/watch?v=u99tNt3TZf8&"
+              />
+            </div>
+          </div>
+          <aside className="w-80 border-l border-zinc-800 bg-zinc-900">
             <ClassItem />
           </aside>
         </main>
